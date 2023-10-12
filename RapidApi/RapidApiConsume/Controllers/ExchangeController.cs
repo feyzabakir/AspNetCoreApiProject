08fa9618dc325565d4e3d5e56259bd89b42b2a31
Feyza Nur Bakır <feyzanurbakirr@gmail.com>
2023-10-12 21:43:27 +0300

    exchange

--- RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
+++ RapidApi/RapidApiConsume/Controllers/ExchangeController.cs
@@ -1,12 +1,37 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RapidApiConsume.Models;
+using System.Linq;
 
 namespace RapidApiConsume.Controllers
 {
     public class ExchangeController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://apidojo-booking-v1.p.rapidapi.com/currency/get-exchange-rates?base_currency=TRY&languagecode=en-us"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "3f71666baamsh8f1c519179ff821p189a44jsnd42329e48d03" },
+        { "X-RapidAPI-Host", "apidojo-booking-v1.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel>(body);
+                return View(values.exchange_rates.ToList());
+            }
         }
     }
 }
